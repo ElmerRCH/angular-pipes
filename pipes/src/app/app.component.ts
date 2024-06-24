806ead203d2a4d1dd274d7d080a6189e78ef2446
Elmer Camacho <renehiguera15@gmail.com>
2024-06-24 15:17:58 -0700

    json

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UpperCasePipe,LowerCasePipe,SlicePipe,CurrencyPipe, PercentPipe} from '@angular/common';
+import { UpperCasePipe,LowerCasePipe,SlicePipe,CurrencyPipe, PercentPipe,JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UpperCasePipe,LowerCasePipe,SlicePipe,CurrencyPipe,PercentPipe],
+  imports: [RouterOutlet,UpperCasePipe,LowerCasePipe,SlicePipe,CurrencyPipe,PercentPipe,JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,5 +15,14 @@ export class AppComponent {
   array = [1,2,3,4,5,6,7,8,9]
   porcentaje = .234
   salario = 12345
+  heroe = {
+    nombre : ' Logan',
+    edad : 500,
+    clave:'Wolverine',
+    direccion:{
+      calle:'Primera',
+      casa:200
+    }
 
+  }
 }
