1da17a0c56d8d9dcc4838ff47c83ba32cde83bb8
Elmer Camacho <renehiguera15@gmail.com>
2024-06-24 14:50:57 -0700

    currency

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UpperCasePipe,LowerCasePipe } from '@angular/common';
+import { UpperCasePipe,LowerCasePipe,SlicePipe,CurrencyPipe, PercentPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,UpperCasePipe,LowerCasePipe],
+  imports: [RouterOutlet,UpperCasePipe,LowerCasePipe,SlicePipe,CurrencyPipe,PercentPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
   nombre: string = 'pruebas'
+  array = [1,2,3,4,5,6,7,8,9]
+  porcentaje = .234
+  salario = 12345
+
 }
